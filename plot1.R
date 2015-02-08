ca4ceae3f713abf31c1fabe7b995bfae2d8e5ae3
Chris Thatcher <thatcher.christopher@gmail.com>
2015-02-08 08:20:28 -0500

    add download.file routine to ensure data is local

--- plot1.R
+++ plot1.R
@@ -2,11 +2,20 @@
 # Week 1
 # plot1.R
 # Chris Thatcher
-#
-# Expects the data is available in `getwd()` as household_power_consumption.txt
-
 library(data.table)
 library(lubridate)
+
+# Expects the data is available in `getwd()` as household_power_consumption.txt
+# Make sure we have the data to work with locally, otherwise go get it.
+data_file = "household_power_consumption.txt"
+data_zip = "household_power_consumption.zip"
+data_url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if( !file.exists(data_file) ){
+    message("Data not present, loading from url.")
+    download.file(data_url, destfile=data_zip, method="curl")
+    unzip(data_zip)
+    file.remove(data_zip)
+}
 
 # fread is fast but its going to force every column to be a character because
 # it can only coerse columns with NA's to character during the ingest.  It also
@@ -14,40 +23,39 @@ library(lubridate)
 # and will deal with the types later
 household_power_consumption = suppressWarnings( fread(
     "household_power_consumption.txt",
-    sep=";",
     na.strings="?"
-));
+))
 
 # convert the Date field to a, uh, Date
 household_power_consumption$Date = parse_date_time(
     household_power_consumption$Date, 
     "%d%m%y"
-);
+)
 
 # convert the Time field to a, uh, Time
 household_power_consumption$Time = parse_date_time(
     household_power_consumption$Time, 
     "%H%M%S"
-);
-    
+)
+
 
 # We will only be using data from the dates 2007-02-01 and 2007-02-02. 
 time_slice = subset(
     household_power_consumption,
     household_power_consumption$Date >= parse_date_time("2007-02-01", "ymd") &
-    household_power_consumption$Date < parse_date_time("2007-02-03", "ymd")
-);
-    
+        household_power_consumption$Date < parse_date_time("2007-02-03", "ymd")
+)
+
 # Finally construct the plot
 with(time_slice, {
-    png('plot1.png');
+    png('plot1.png')
     hist( 
         as.numeric(Global_active_power), 
         main="Global Active Power",
         xlab="Global Active Power (kilowatts)",
         col="red"
     );
-    dev.off();
+    dev.off()
 });
 
 
